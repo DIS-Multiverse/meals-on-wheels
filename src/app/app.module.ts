f0c76f78ac3f1536365c11da5e3ce6e05ef641b3
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-03-22 15:12:16 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ViewClientsComponent } from './view-clients/view-clients.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ViewClientsComponent
   ],
   imports: [
     BrowserModule,
