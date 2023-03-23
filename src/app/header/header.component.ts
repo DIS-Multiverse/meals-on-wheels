4ba231dde0330cc08e0233504cbd64f914d2d169
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-03-23 10:59:39 +0000

    add logo and title font

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faUtensils, faTruckFast } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  public utensilsIcon = faUtensils;
+  public truckIcon = faTruckFast;
 
 }
