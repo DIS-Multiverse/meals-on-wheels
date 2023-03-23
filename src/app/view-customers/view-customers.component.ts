d7386c1477d85da6d75d493d8e536e635cc05c87
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-03-23 11:29:55 +0000

    add view-customers ts

--- src/app/view-customers/view-customers.component.ts
+++ src/app/view-customers/view-customers.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/users';
 
 @Component({
   selector: 'app-view-customers',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-customers.component.css']
 })
 export class ViewCustomersComponent {
+  title = 'Users.UI';
+  users: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() : void {
+    this.userService.getUsers().subscribe((result: User[]) => (this.users = result));
+  }
 }
