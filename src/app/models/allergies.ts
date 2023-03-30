10d77e99286f033cafd32d762c179cff75000df5
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-03-30 14:51:52 +0100

    Add allergy checkbox logic

--- src/app/models/allergies.ts
+++ src/app/models/allergies.ts
@@ -1,10 +1,12 @@
-export const Allergies: string[] = [
-  'Eggs',
-  'Fish',
-  'Milk',
-  'Peanuts',
-  'Shellfish (e.g. crab, lobster, shrimp)',
-  'Soybeans',
-  'Tree nuts (e.g. almonds, walnuts, pecans)',
-  'Wheat'
+import { Allergy } from "./allergy";
+
+export const Allergies: Allergy[] = [
+  {id: 1, name: 'Eggs', isSelected: false},
+  {id: 2, name: 'Fish', isSelected: false},
+  {id: 3, name: 'Milk', isSelected: false},
+  {id: 4, name: 'Peanuts', isSelected: false},
+  {id: 5, name: 'Shellfish (e.g. crab, lobster, shrimp)', isSelected: false},
+  {id: 6, name: 'Soybeans', isSelected: false},
+  {id: 7, name: 'Tree nuts (e.g. almonds, walnuts, pecans)', isSelected: false},
+  {id: 8, name: 'Wheat', isSelected: false},
 ]
