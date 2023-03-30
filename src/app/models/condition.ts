005f157a30f30b074f07ee708054811b57b2f745
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-03-30 15:45:41 +0100

    Add Customer- Condition logic

---
+++ src/app/models/condition.ts
@@ -0,0 +1,5 @@
+export interface Condition {
+  id: number;
+  name: string;
+  isSelected: boolean;
+}
--- src/app/models/conditions.ts
+++ src/app/models/conditions.ts
@@ -1,6 +1,8 @@
-export const Conditions: string[] = [
-  'Blind',
-  'Deaf',
-  'Dementia',
-  'Diabetes'
+import { Condition } from "./condition";
+
+export const Conditions: Condition[] = [
+  {id: 1, name: 'Blind', isSelected: false},
+  {id: 2, name: 'Deaf', isSelected: false},
+  {id: 3, name: 'Dementia', isSelected: false},
+  {id: 4, name: 'Diabetes', isSelected: false}
 ]
